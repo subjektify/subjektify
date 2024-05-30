eb7bb969abd60cffeb798cf5ccfd27cb053d6cdf
Mohamed Amer <139323704+atmohamedamer@users.noreply.github.com>
2024-05-29 23:24:43 -0700

    Update init.ts

--- src/cli/init.ts
+++ src/cli/init.ts
@@ -3,43 +3,52 @@ import { Prompt } from "./prompt";
 
 export const init = async (args: any): Promise<void> => {
     Log.verbose("Initializing a new namespace.");
-    const { answer, namespace } = await promptUser();
+    const answer = await promptChoice();
 
     switch (answer) {
         case "javascript":
-            return createJavascriptNamespace(namespace);
+            return createJavascriptNamespace();
         case "typescript":
-            return createTypescriptNamespace(namespace);
+            return createTypescriptNamespace();
         case "empty":
-            return createEmptyConfig(namespace);
+            return createEmptyConfig();
+        case "quit":
+            return;
     }
 }
 
-const promptUser = async () => {
+const promptChoice = async () => {
     const choices = [
         { message: "Create a Javascript namespace", name: "javascript" },
         { message: "Create a Typescript namespace", name: "typescript" },
-        { message: "Create an empty subjektify.config.js", name: "empty" }
+        { message: "Create an empty subjektify.config.js", name: "empty" },
+        { message: "Quit", name: "quit"}
     ]
 
     const answer = await Prompt.select("What do you want to do?", choices);
+    Log.verbose(`User selected: ${answer}`);
+    return answer;
+}
+
+const promptNamespace = async () => {
     const namespace = await Prompt.input("Enter a name for the new namespace:") || "my-namespace";
-    Log.verbose(`User answered: ${answer} with namespace: ${namespace}`);
-
-    return { answer, namespace };
+    Log.verbose(`Selected namespace: ${namespace}`);
+    return namespace;
 }
 
-const createJavascriptNamespace = async (namespace: string) => {
+const createJavascriptNamespace = async () => {
     Log.verbose("Creating a new Javascript namespace.");
+    const namespace = await promptNamespace();
     // Create a new namespace with a default configuration
 }
 
-const createTypescriptNamespace = async (namespace: string) => {
+const createTypescriptNamespace = async () => {
     Log.verbose("Creating a new Typescript namespace.");
+    const namespace = await promptNamespace();
     // Create a new namespace with a default configuration
 }
 
-const createEmptyConfig = async (namespace: string) => {
+const createEmptyConfig = async () => {
     Log.verbose("Creating an empty configuration file.");
     // Create an empty configuration file
 }
