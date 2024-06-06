a06f0aee30334ce8d0d1447b42ad5609642af7d7
Mohamed Amer <139323704+atmohamedamer@users.noreply.github.com>
2024-06-06 05:15:42 -0700

    Update codegen types

--- packages/subjektify-codegen/src/index.ts
+++ packages/subjektify-codegen/src/index.ts
@@ -1,6 +1,7 @@
 import { Log, SubjektifyRuntimeEnvironment, extendEnvironment, task } from "subjektify";
 
 import "./type-extensions";
+import { subjektifyBuildTask } from "@subjektifylabs/subjektify-build";
 
 // TODO: Remove after adding development environment.
 Log.setVerbose(true);
@@ -10,4 +11,5 @@ extendEnvironment((sre) => {
 });
 
 task("codegen", "Builds your Subjekt model and adds the artifacts to the runtime environment", async (_, sre) => {
+    await subjektifyBuildTask(_, sre);
 });
--- packages/subjektify-codegen/src/type-extensions.ts
+++ packages/subjektify-codegen/src/type-extensions.ts
@@ -1,5 +1,17 @@
 import { SubjektModel } from "subjekt";
-import "subjektify/dist/types/runtime"
+import "subjektify/dist/types/config";
+import "subjektify/dist/types/runtime";
+
+export interface CodeGenConfig {
+    target: string;
+}
+
+declare module "subjektify/dist/types/config" {
+
+    export interface SubjektifyConfig {
+        codegen: string[];
+    }
+}
 
 declare module "subjektify/dist/types/runtime" {
 
