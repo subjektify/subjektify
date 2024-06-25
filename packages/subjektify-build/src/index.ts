d5481a9e9d733671a3d7089f54e4a8bd6ec8758d
Mohamed Amer <139323704+atmohamedamer@users.noreply.github.com>
2024-06-25 04:18:06 -0700

    Update index.ts

--- packages/subjektify-build/src/index.ts
+++ packages/subjektify-build/src/index.ts
@@ -1,9 +1,9 @@
-import { extendEnvironment, task } from "subjektify";
+import { extendEnvironment, SubjektifyRuntimeEnvironment, task } from "subjektify";
 import { subjektifyBuildTask } from "./core/build";
 
 import "./core/types";
 
-extendEnvironment((sre) => {
+extendEnvironment((sre: SubjektifyRuntimeEnvironment) => {
     sre.model = {
         ast: {},
         semantic: {},
@@ -11,8 +11,8 @@ extendEnvironment((sre) => {
     };
 });
 
-task("build", "Builds your Subjekt model and adds the artifacts to the runtime environment", async (_, sre) => {
-    await subjektifyBuildTask(_, sre);
+task("build", "Builds your Subjekt model and adds the artifacts to the runtime environment", async (taskArguments: any, sre: SubjektifyRuntimeEnvironment) => {
+    await subjektifyBuildTask(taskArguments, sre);
 });
 
 export * from "./core/types";
