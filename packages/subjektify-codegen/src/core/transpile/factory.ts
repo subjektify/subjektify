c7e603a1500c7d0113245816ca86a37ccd6cf5dd
Mohamed Amer <139323704+atmohamedamer@users.noreply.github.com>
2024-07-11 01:12:39 -0700

    Update codegen transpilation

--- packages/subjektify-codegen/src/core/transpile/factory.ts
+++ packages/subjektify-codegen/src/core/transpile/factory.ts
@@ -1,8 +1,54 @@
 import { CodeGenerator } from "../generate/targets";
-import { TypescriptTranspiler } from "./typescript";
+import { CodeTranspiler } from "./base";
+import { SolidityTranspiler } from "./solidity";
+import { TypescriptClientTranspiler, TypescriptServerTranspiler } from "./typescript";
 
 export class TranspilerFactory {
-    static transpiler(generator: CodeGenerator) {
-        return new TypescriptTranspiler(generator);
+    static transpiler(generator: CodeGenerator): CodeTranspiler {
+        const codegenConfig = generator.config;
+        switch (codegenConfig.target) {
+            case 'client':
+                return this._clientTranspiler(generator);
+            case 'contract':
+                return this._contractTranspiler(generator);
+            case 'server':
+                return this._serverTranspiler(generator);
+            default:
+                throw new Error(`Unknown target: ${codegenConfig.target}`);
+        }
+    }
+
+    private static _clientTranspiler(generator: CodeGenerator): CodeTranspiler {
+        const codegenConfig = generator.config;
+        switch (codegenConfig.language) {
+            case 'typescript':
+                return new TypescriptClientTranspiler(generator);
+            default:
+                this._throwUnknownLanguageError(codegenConfig.language);
+        }
+    }
+
+    private static _contractTranspiler(generator: CodeGenerator): CodeTranspiler {
+        const codegenConfig = generator.config;
+        switch (codegenConfig.language) {
+            case 'typescript':
+                return new SolidityTranspiler(generator);
+            default:
+                this._throwUnknownLanguageError(codegenConfig.language);
+        }
+    }
+
+    private static _serverTranspiler(generator: CodeGenerator): CodeTranspiler {
+        const codegenConfig = generator.config;
+        switch (codegenConfig.language) {
+            case 'typescript':
+                return new TypescriptServerTranspiler(generator);
+            default:
+                this._throwUnknownLanguageError(codegenConfig.language);
+        }
+    }
+
+    private static _throwUnknownLanguageError(language: string): never {
+        throw new Error(`Unknown language: ${language}`);
     }
 }
